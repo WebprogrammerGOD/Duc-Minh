8d1d90aa8b88679b2efd205b740dc581c604c3d5
WebProgrammerGod <minhcgd1401@gmail.com>
2024-08-25 22:08:54 +0700

    update

--- Main/js/Testing.js
+++
@@ -1,8 +0,0 @@
-/*General*/
-const API_KEY = "bcadcc57-8163-41dd-9981-284fa8ff927e";
-const DEFAULT_VALUE = __
-/*Weather status*/
-const searchInput = document.getElementById("search");
-const country = document.querySelector("countrySearchInput");
-const state = document.querySelector("stateSearchInput");
-const city = document.querySelector("citySearchInput");
