7575a06626aab57cd2f35e0d224044ea9aa9b060
WebProgrammerGod <minhcgd1401@gmail.com>
2024-08-26 19:46:00 +0700

    update

--- Main/js/App.js
+++ Main/js/App.js
@@ -3,9 +3,8 @@ const API_KEY = "6ab9ff8701516ad788b22be4fd93b362";
 const DEFAULT_VALUE = "__";
 /*Weather status*/
 const searchInput = document.getElementById("search");
-const cityName = document.querySelector("searchInput");
+const cityName = document.querySelector(".city-name");
 const weatherState = document.querySelector(".weather-state");
-const weatherIcon = document.querySelector(".weather-icon");
 const temperature = document.querySelector(".temperature");
 const sunrise = document.querySelector(".sunrise");
 const sunset = document.querySelector(".sunset");
@@ -13,12 +12,12 @@ const humidity = document.querySelector(".humidity");
 const windSpeed = document.querySelector(".wind-speed");
 /*Fetch data*/
 searchInput.addEventListener("change", (event) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=ru&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=vi&units=metric`)
     .then((response) => response.json())
     .then((data) => {
         cityName.innerHTML = data.name || DEFAULT_VALUE;
         weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
-        weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` || DEFAULT_VALUE;
         temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+        sunrise.innerHTML = data.weather[0].sunrise || DEFAULT_VALUE; 
     })
 })
