f5c3a8d84dc33f7a1dcea589c1ae890408d2537f
WebProgrammerGod <minhcgd1401@gmail.com>
2024-10-14 20:08:32 +0700

    v1.1

--- Main/js/App.js
+++ Main/js/App.js
@@ -15,8 +15,20 @@ searchInput.addEventListener("change", (event) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=en&units=metric`)
     .then((response) => response.json())
     .then((data) => {
+        if (data.cod == '404'){
+            alert('Cannot find the city');
+            return;
+        }
+        else if (data.cod == '429'){
+            alert('The API is on cooldown');
+            return;
+        }
         cityName.innerHTML = data.name || DEFAULT_VALUE;
         weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
         temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+        sunrise.innerHTML = new Date(data.sys.sunrise * 1000).toLocaleTimeString() || DEFAULT_VALUE;
+        sunset.innerHTML = new Date(data.sys.sunset * 1000).toLocaleTimeString() || DEFAULT_VALUE;
+        humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+        windSpeed.innerHTML = Math.round(data.wind.speed) || DEFAULT_VALUE;
     })
 })
