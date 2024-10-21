bf1161238047c774db8c0d07743ab4091426912b
WebProgrammerGod <minhcgd1401@gmail.com>
2024-10-21 19:08:53 +0700

    v1.03.5

--- Main/js/App.js
+++ Main/js/App.js
@@ -15,14 +15,16 @@ searchInput.addEventListener("change", (event) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=en&units=metric`)
     .then((response) => response.json())
     .then((data) => {
+        /*Return error messages*/
         if (data.cod == '404'){
             alert('Cannot find the city');
             return;
         }
         else if (data.cod == '429'){
-            alert('The API is on cooldown');
+            alert('The forecast is on cooldown');
             return;
         }
+        /*Return data*/
         cityName.innerHTML = data.name || DEFAULT_VALUE;
         weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
         temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
