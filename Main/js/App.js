52360eb6d0cf7594e16013ce63f3c1479ed1c997
WebProgrammerGod <minhcgd1401@gmail.com>
2024-11-25 20:57:27 +0700

    v1.1.01

--- Main/js/App.js
+++ Main/js/App.js
@@ -1,15 +1,19 @@
 // General
 const API_KEY = "6ab9ff8701516ad788b22be4fd93b362";
 const DEFAULT_VALUE = "__";
+
 // Weather status
 const searchInput = document.getElementById("search");
 const cityName = document.querySelector(".city-name");
 const weatherState = document.querySelector(".weather-state");
 const temperature = document.querySelector(".temperature");
+const feel = document.querySelector(".feel-like");
+
 const sunrise = document.querySelector(".sunrise");
 const sunset = document.querySelector(".sunset");
 const humidity = document.querySelector(".humidity");
 const windSpeed = document.querySelector(".wind-speed");
+
 // Fetch data
 searchInput.addEventListener("change", (event) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=en&units=metric`)
@@ -20,14 +24,18 @@ searchInput.addEventListener("change", (event) => {
             alert('Cannot find the city');
             return;
         }
+
         else if (data.cod == '429'){
             alert('The forecast is on cooldown');
             return;
         }
+
         // Return data
         cityName.innerHTML = data.name || DEFAULT_VALUE;
         weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
         temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+        feel.innerHTML = Math.round(data.main.feels_like) || DEFAULT_VALUE;
+
         sunrise.innerHTML = new Date(data.sys.sunrise * 1000).toLocaleTimeString() || DEFAULT_VALUE;
         sunset.innerHTML = new Date(data.sys.sunset * 1000).toLocaleTimeString() || DEFAULT_VALUE;
         humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
